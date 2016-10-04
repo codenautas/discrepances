e9963c5ba3b487a812dd5be2d726691980ba0598
Diego F.(EW7) <diegoefe@gmail.com>
2016-10-04 09:38:38 -0300

    Date/RegExp

--- test/test.js
+++ test/test.js
@@ -4,13 +4,15 @@ var JSON4all = require('json4all');
 var expect = require('expect.js');
 var discrepances = require('../lib/discrepances.js');
 
+var fechaActual = new Date();
+
 describe("discrepances", function(){
     [
         {a:4                 , b:4                  , expected: null                                                                      },
         {a:4                 , b:5                  , expected: {difference:-1             , values:[4, 5]                               }},
         {a:"4"               , b:4                  , expected: {types:['string', 'number'], values:["4", 4]                             }},
         {a:null              , b:0                  , expected: {types:['null'  , 'number'], values:[null, 0]                            }},
-        {skip:true, a:new Date()        , b:/a/                , expected: {class:['Date'  , 'RegExp'], values:[new Date(), /a/]                    }},
+        {a:fechaActual       , b:/a/                , expected: {classes:['Date'  , 'RegExp'], values:[fechaActual, /a/]                    }},
         {skip:true, a:new Date(2011,1,3), b:new Date(2011,1,4) , expected: {difference:'a definir', values:[new Date(2011,1,3), new Date(2011,1,4)] }},
         {skip:true, a:[1,2,3,4,5]       , b:[1,2,33,4,5,6]     , expected: {array:{length:discrepances(5,6), 2:discrepances(3,33)}                  }},
         {skip:true, a:{x:1, y:2}        , b:{y:3, z:{zz:3}}    , expected: {object:{x:{onlyLeft:1}, y:discrepances(2,3), z:{onlyRight:{zz:3}}}      }},
