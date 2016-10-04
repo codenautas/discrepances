8420a079655160567e20eb0b8003357dff17ffa3
Diego F.(EW7) <diegoefe@gmail.com>
2016-10-04 13:41:33 -0300

    Primeros tests con strings

--- test/test.js
+++ test/test.js
@@ -8,16 +8,24 @@ var fechaActual = new Date();
 
 describe("discrepances", function(){
     [
-        {a:4                 , b:4                  , expected: null                                                                      },
-        {a:4                 , b:5                  , expected: {difference:-1             , values:[4, 5]                               }},
-        {a:"4"               , b:4                  , expected: {types:['string', 'number'], values:["4", 4]                             }},
-        {a:null              , b:0                  , expected: {types:['null'  , 'number'], values:[null, 0]                            }},
-        {a:fechaActual       , b:/a/                , expected: {classes:['Date'  , 'RegExp'], values:[fechaActual, /a/]                    }},
-        {skip:true, a:new Date(2011,1,3), b:new Date(2011,1,4) , expected: {difference:'a definir', values:[new Date(2011,1,3), new Date(2011,1,4)] }},
-        {a:[1,2,3,4,5]       , b:[1,2,33,4,5,6]     , expected: {array:{length:discrepances(5,6), 2:discrepances(3,33)}                  }},
-        {a:[1,2,3,4,5]       , b:[1,2,3,4,5]     , expected: null                  },
-        {a:{x:1, y:2}        , b:{y:3, z:{zz:3}}    , expected: {object:{x:{onlyLeft:1}, y:discrepances(2,3), z:{onlyRight:{zz:3}}}      }},
-        {a:{x:1, y:2, z:[3]} , b:{x:1, y:2, z:[3]}  , expected: null                                                                      },
+        {a:4                 , b:4                  , expect: null                                                                      },
+        {a:4                 , b:5                  , expect: {difference:-1             , values:[4, 5]                               }},
+        {a:"4"               , b:4                  , expect: {types:['string', 'number'], values:["4", 4]                             }},
+        {a:null              , b:0                  , expect: {types:['null'  , 'number'], values:[null, 0]                            }},
+        {a:fechaActual       , b:/a/                , expect: {classes:['Date'  , 'RegExp'], values:[fechaActual, /a/]                    }},
+        {a:[1,2,3,4,5]       , b:[1,2,33,4,5,6]     , expect: {array:{length:discrepances(5,6), 2:discrepances(3,33)}                  }},
+        {a:[1,2,3,4,5]       , b:[1,2,3,4,5]        , expect: null                  },
+        {a:{x:1, y:2}        , b:{y:3, z:{zz:3}}    , expect: {object:{x:{onlyLeft:1}, y:discrepances(2,3), z:{onlyRight:{zz:3}}}      }},
+        {a:{x:1, y:2, z:[3]} , b:{x:1, y:2, z:[3]}  , expect: null                                                                      },
+        {a:"un string"       , b:"un string"        , expect: null                                                                      },
+        {a:"un negro pez"    , b:"un blanco pez"    , expect: {difference: {left:'negro', right:'blanco', pos:3}}                        },
+        {skip:true, a:new Date(2011,1,3), b:new Date(2011,1,4) , expect: {difference:'a definir', values:[new Date(2011,1,3), new Date(2011,1,4)] }},
+        {skip:true, a: new Date(1992,11,5)       , b:new Date(1935,8,1)         , expect:'1992-12-05 != 1935-09-01'},
+        {skip:true, a: new Date(1992,11,5,10,0,0), b:new Date(1935,8,1,15,0,0)  , expect:'1992-12-05 10:00:00 != 1935-09-01 15:00:00'},
+        {skip:true, a: new Date(1992,11,5,15,0,0), b:new Date(1992,11,5,10,10,0), expect:'04:50:00'},
+        {skip:true, a: new Date(1992,11,5,0,0,0),  b:new Date(1992,11,6,15,25,0), expect:'1992-12-05 00:00:00 != 1992-12-06 15:25:00 => -39:25:00'},
+        {skip:true, a: new Date(1992,11,5,0,0,0,100),  b:new Date(1992,11,6,15,25,0,200), expect:'1992-12-05 00:00:00.100 != 1992-12-06 15:25:00.200 => -39:25:00.100'},
+        {skip:true, a: new Date(1462670136585+100250), b:new Date(1462670136585), expect:'00:01:40.250'},
     ].forEach(function(fixture){
         if(fixture.skip) {
             delete fixture.skip;
@@ -25,10 +33,11 @@ describe("discrepances", function(){
             return true;
         }
         it("fixture: "+JSON.stringify(fixture), function(){
-            //console.log("RES", JSON4all.stringify(discrepances(fixture.a, fixture.b))); console.log("EXP", JSON4all.stringify(fixture.expected));
-            expect(discrepances(fixture.a, fixture.b)).to.eql(fixture.expected);
-            expect(JSON.stringify(discrepances(fixture.a, fixture.b))).to.eql(JSON.stringify(fixture.expected));
-            expect(JSON4all.stringify(discrepances(fixture.a, fixture.b))).to.eql(JSON4all.stringify(fixture.expected));
+            // console.log("RES", JSON4all.stringify(discrepances(fixture.a, fixture.b)));
+            // console.log("EXP", JSON4all.stringify(fixture.expect));
+            expect(discrepances(fixture.a, fixture.b)).to.eql(fixture.expect);
+            expect(JSON.stringify(discrepances(fixture.a, fixture.b))).to.eql(JSON.stringify(fixture.expect));
+            expect(JSON4all.stringify(discrepances(fixture.a, fixture.b))).to.eql(JSON4all.stringify(fixture.expect));
         });
     });
 });
