789479b2c21342216b4a6ff7c370b1c36d5730f9
Diego F.(EW7) <diegoefe@gmail.com>
2016-10-04 09:28:34 -0300

    null/number

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ describe("discrepances", function(){
         {a:4                 , b:4                  , expected: null                                                                      },
         {a:4                 , b:5                  , expected: {difference:-1             , values:[4, 5]                               }},
         {a:"4"               , b:4                  , expected: {types:['string', 'number'], values:["4", 4]                             }},
-        {skip:true, a:null              , b:0                  , expected: {types:['null'  , 'number'], values:[null, 0]                            }},
+        {a:null              , b:0                  , expected: {types:['null'  , 'number'], values:[null, 0]                            }},
         {skip:true, a:new Date()        , b:/a/                , expected: {class:['Date'  , 'RegExp'], values:[new Date(), /a/]                    }},
         {skip:true, a:new Date(2011,1,3), b:new Date(2011,1,4) , expected: {difference:'a definir', values:[new Date(2011,1,3), new Date(2011,1,4)] }},
         {skip:true, a:[1,2,3,4,5]       , b:[1,2,33,4,5,6]     , expected: {array:{length:discrepances(5,6), 2:discrepances(3,33)}                  }},
